6183bef5c6efc92d58a8203643288579a3aa6c65
gulfeyzaciftci <gulfeyza333@gmail.com>
2023-02-16 12:20:09 +0100

    showImage

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react";
 import { Container, Image } from "react-bootstrap";
 
 const Language = ({ name, img, options }) => {
+  const [showImage, setShowImage] = useState(true);
+
   return (
-    <Container>
-      <Container>
-        <Image src={img} width="70%"></Image>
-        <h3 className="display-6">{name}</h3>
-      </Container>
-      <ol>
-        {options.map((item) => {
-          return <li>{item}</li>;
-        })}
-      </ol>
+    <Container
+      className="p-4 rounded-2 h-100 lang-card"
+      style={{ background: "peachpuff", height: "250px" }}
+      onClick={() => setShowImage(!showImage)}
+      type="button"
+    >
+      {showImage && (
+        <Container>
+          <Image src={img} width="70%" className="lang-logo"></Image>
+          <h3 className="display-6 m-4">{name}</h3>
+        </Container>
+      )}
+      {!showImage && (
+        <ol className="h-100 d-flex flex-column justify-content-center h-100">
+          {options.map((item) => {
+            return <li className="h5 text-start">{item}</li>;
+          })}
+        </ol>
+      )}
     </Container>
   );
 };
